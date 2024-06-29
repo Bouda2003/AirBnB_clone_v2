f0edaa3c7a970ca8b075ec957166b6c8e8a1d4d2
Abdelrahman_Mohsen <aute.mohsen@gmail.com>
2024-06-28 19:09:32 -0700

    console

--- console.py
+++ console.py
@@ -115,39 +115,41 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
-    def do_create(self, line):
+    def do_create(self, arg):
         """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
         Create a new class instance with given keys/values and print its id.
         """
-        try:
-            if not line:
-                raise SyntaxError()
-            my_list = line.split(" ")
-
-            kwargs = {}
-            for i in range(1, len(my_list)):
-                key, value = tuple(my_list[i].split("="))
-                if value[0] == '"':
-                    value = value.strip('"').replace("_", " ")
-                else:
-                    try:
-                        value = eval(value)
-                    except (SyntaxError, NameError):
-                        continue
-                kwargs[key] = value
-
-            if kwargs == {}:
-                obj = eval(my_list[0])()
-            else:
-                obj = eval(my_list[0])(**kwargs)
-                storage.new(obj)
-            print(obj.id)
-            obj.save()
-
-        except SyntaxError:
+        Tokens = arg.split(" ")
+        if len(Tokens) == 0:
             print("** class name missing **")
-        except NameError:
+        elif Tokens[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
+        else:
+             kwargs = {}
+        for token in Tokens[1:]:
+            if "=" not in token:
+                continue
+            key, value = token.split("=", 1)
+            if value.startswith('"') and value.endswith('"'):
+                value = value[1:-1].replace('_', ' ').replace('\\"', '"')
+            else:
+                try:
+                    if '.' in value:
+                        value = float(value)
+                    else:
+                        value = int(value)
+                except ValueError:
+                    continue
+
+            kwargs[key] = value
+
+        if kwargs:
+            new_instance = HBNBCommand.classes[class_name](**kwargs)
+        else:
+            new_instance = HBNBCommand.classes[class_name]()
+
+        new_instance.save()
+        print(new_instance.id)
 
     def help_create(self):
         """ Help information for the create method """
